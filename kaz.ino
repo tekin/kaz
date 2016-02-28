a6cd55448da269e462873c7a8004afc66accacca
Tekin Suleyman <tekin@tekin.co.uk>
2016-02-28 12:02:54 +0000

    Wire in stepper motor
    This code assumes the Twiz-manager software has been set up to send only the
    rotational z euler angle as a decimal (i.e. custom content of "rz_d" which
    sends a value between 0.0 and 1.0). The arduino will expect the data on port
    10000
    
    The code also attempts some very crude smoothing by only updating the stepper
    motor if the difference in the reading from the previous value is greater than
    0.05, which greatly reduces the fidility, but results in less jittery motion.

--- kaz.ino
+++ kaz.ino
@@ -3,6 +3,7 @@
 #include <EthernetUdp.h>
 #include <OSCBundle.h>
 #include "EulerStepCounter.h"
+#include <AccelStepper.h>
 
 // you can find this written on the board of some Arduino Ethernets or shields
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED } ;
@@ -10,30 +11,33 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED } ;
 //       Ethernet shield.
 byte ip[] = { 192, 168, 1, 100 };
 
-int serverPort = 10000; //TouchOSC (incoming port)
-//Create UDP message object
+//TouchOSC (incoming port)
+int serverPort = 10000;
 EthernetUDP Udp;
 EulerStepCounter stepCounter;
+AccelStepper stepper1(1, 7, 6);
+float previous_reading = 0;
 
 void setup() {
-  Serial.begin(250000);
   Ethernet.begin(mac, ip);
   Udp.begin(serverPort);
+  stepper1.setMaxSpeed(900.0);
+  stepper1.setAcceleration(900.0);
 }
 
 void loop() {
   float reading = parseOSCMessage();
 
-  if( reading != -1.0 )
+  if( reading != -1.0  && abs(reading - previous_reading) > 0.05) {
     stepCounter.update(reading);
+    float position = stepCounter.linear_position();
+    // adjust for the steppers scale (200 per revolution)
+    int linear_position = (int)(position * 200 * -1);
+    stepper1.moveTo(linear_position);
+  }
+  previous_reading = reading;
 
-
-  Serial.print("Euler:   ");
-  Serial.println(reading);
-  Serial.print("Stepper: ");
-  Serial.println(stepCounter.linear_position());
-
-  delay(50);
+  stepper1.run();
 }
 
 // either returns the euler angle or -1.0 if packet not received
