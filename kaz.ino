fc2808f53ed177ef7640ba392f9d7b4b9663395f
Tekin Suleyman <tekin@tekin.co.uk>
2016-02-28 12:28:30 +0000

    Add method for smoothing the position calculation
    We calculate the running average of the last 5 readings to compensate for
    jitters in the sensor readings.

--- kaz.ino
+++ kaz.ino
@@ -28,16 +28,37 @@ void setup() {
 void loop() {
   float reading = parseOSCMessage();
 
-  if( reading != -1.0  && abs(reading - previous_reading) > 0.05) {
+  if( reading != -1.0 ) {
     stepCounter.update(reading);
     float position = stepCounter.linear_position();
     // adjust for the steppers scale (200 per revolution)
     int linear_position = (int)(position * 200 * -1);
-    stepper1.moveTo(linear_position);
+    stepper1.moveTo(runningAverage(linear_position));
   }
   previous_reading = reading;
 
   stepper1.run();
+}
+
+long runningAverage(int M) {
+  #define READINGS_COUNT 5
+  static int LM[READINGS_COUNT];
+  static byte index = 0;
+  static long sum = 0;
+  static byte count = 0;
+
+  // for speed we keep a running tally rather than
+  // adding all the elements each time. Not a huge
+  // deal with a small READINGS_COUNT, but still.
+  sum -= LM[index];
+
+  LM[index] = M;
+  sum += LM[index];
+  index++;
+  index = index % READINGS_COUNT;
+  if (count < READINGS_COUNT) count++;
+
+  return sum / count;
 }
 
 // either returns the euler angle or -1.0 if packet not received
